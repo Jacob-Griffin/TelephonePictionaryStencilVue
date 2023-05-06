4be622465c41227bf36bdfa8ea643f1133bf6e4b
jacob-griffin <jacob@griffinspride.com>
2023-05-05 18:19:38 -0700

    Added templates for the private firebase files

---
+++ apps/tp-app/firebase.config.template.js
@@ -0,0 +1,11 @@
+//Rename this file 'firebase.config.js' to start using it
+export default {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+  databaseURL: "",
+};
