ab825b3f0aa989fc79dd435e729ab6e22dd776bc
jacob-griffin <jacob@griffinspride.com>
2023-06-13 23:58:00 -0700

    Redo private files

--- apps/tp-app/firebase.config.js
+++ apps/tp-app/firebase.config.js
@@ -1,11 +1,12 @@
-//Make sure these environment variables are defined in a .env file at the root
+import {apiKey,authDomain,projectId,storageBucket,messagingSenderId,appId,measurementId,databaseURL} from './firebase.secrets.js'
+
 export default {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
-  measurementId: import.meta.env.VITE_MEASUREMENTID,
-  databaseURL: import.meta.env.VITE_DATABASEURL,
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+  databaseURL,
 };
