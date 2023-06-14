de70dec932ef0628a664a367cd2e109f930e23b0
jacob-griffin <jacob@griffinspride.com>
2023-06-13 22:56:45 -0700

    Switch to github automation

---
+++ apps/tp-app/firebase.config.js
@@ -0,0 +1,11 @@
+//Make sure these environment variables are defined in a .env file at the root
+export default {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
+  databaseURL: import.meta.env.VITE_DATABASEURL,
+};
