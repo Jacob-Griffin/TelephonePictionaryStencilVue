6f0fc4983cfe78f5a59f65855299a0c31a515dfd
jacob-griffin <jacob@griffinspride.com>
2024-07-21 12:52:11 -0700

    component generate script for lit

---
+++ packages/byfo-components-lit/generate-element.js
@@ -0,0 +1,94 @@
+import {exec,execSync} from 'child_process';
+
+const [_1,_2,cname,...args] = process.argv;
+if(cname === undefined){
+  console.error('Args Error: No component name is given');
+  process.exit(1);
+}
+const componentName = {};
+componentName.normalized = cname.replace(/^byfo-?/,'').replace(/[A-Z]/,match => `-${match.toLowerCase()}`);
+componentName.class = 'Byfo'+componentName.normalized.replace(/(?:^|-)([a-z])/,(_,letter)=>letter.toUpperCase());
+componentName.tagname = 'byfo-'+componentName.normalized;
+componentName.file = `./src/${componentName.tagname}.ts`
+
+try {
+  //Weird backwards try. If the file successfully reads, then we have a problem because the component already exists
+  execSync(`cat ${componentName.file} 2>/dev/null`);
+  console.error(`Error: ${componentName.tagname} already exists`);
+  process.exit(1);
+} catch (e) {
+  //Success! There wasn't a file to read.
+  //If the error was *not* file not found, then that'll show itself later
+}
+
+//Step 1: create the boilerplate string
+const fileContents = `import { LitElement, css, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+/**
+ * Description of your element here. Use @property doc tags to describe props
+ */
+@customElement('${componentName.tagname}')
+export class ${componentName.class} extends ${componentName.parent ?? 'LitElement'} {
+  render() {
+    return html\\\`\\\`;
+  }
+  static styles = css\\\`
+    :host {
+      display: block;
+    }
+  \\\`;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    '${componentName.tagname}': ${componentName.class};
+  }
+}`;
+
+//Step 2: Write the file contents
+execSync(`cat > ${componentName.file} <<< "${fileContents}"`);
+
+//Step 3: Modify the package.json
+const contents = execSync('cat ./package.json');
+if(typeof contents.toString?.() !== 'string'){
+  console.error('File error: cannot read package.json');
+  process.exit(1);
+}
+
+let packageJson = JSON.parse(contents.toString());
+packageJson.exports[`./${componentName.normalized}`] = componentName.file;
+
+const formatJson = (json,depth = 1) => {
+  let output = `{\n`;
+  Object.keys(json).forEach(key => {
+    output += '  '.repeat(depth);
+    output += `\\"${key}\\": `;
+    switch(typeof json[key]){
+      case 'string':
+        output += `\\"${json[key]}\\"`;
+        break;
+      case 'number':
+        output += `${json[key]}`;
+        break;
+      case 'boolean':
+        output += json[key] ? 'true' : 'false';
+        break;
+      case 'object':
+        output += formatJson(json[key],depth+1);
+        break;
+    }
+    output += ',\n';
+  });
+  if(depth > 1){
+    output += '  '.repeat(depth - 1);
+  }
+  output += '}';
+  output = output.replaceAll(/,(?=\s+})/g,'');
+  return output;
+}
+
+const newContents = formatJson(packageJson);
+
+//Step 4: Write the modified package json to file
+execSync(`cat > ./package.json <<< "${newContents}"`)
