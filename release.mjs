b64f1d8ac6c287d1753b5e77bd00f51a0d228968
jacob-griffin <jacob@griffinspride.com>
2024-08-02 22:19:41 -0700

    More release script progress

--- release.mjs
+++ release.mjs
@@ -1,5 +1,30 @@
 import { processArgs } from "./script-utils.mjs";
+import { execSync } from 'child_process';
 
 const args = processArgs(process.argv,[],['v']);
 const version = args.get('v');
 console.log(`Releasing version ${version}`);
+
+const packages = execSync("find packages/*/package.json apps/*/package.json ./package.json -iname package.json").toString().trim().split('\n');
+
+packages.forEach(p => {
+  const oldContent = execSync(`cat ${p}`).toString();
+  const newContent = oldContent.replace(/"version": ".*"/,`"version": "${version}"`);
+  execSync(`echo -n "${newContent.replaceAll('\\"','\\\\"').replaceAll('"','\\"')}" | cat > ${p}`);
+})
+
+const _branch = execSync('git branch').toString();
+const branch = _branch.match(/\* (.+)/)[1];
+console.log(branch);
+if(branch !== 'dev'){
+  console.log('switching to branch dev')
+  execSync(`git switch dev`);
+}
+
+packages.forEach(p => execSync(`git add ${p}`));
+execSync(`git commit -m "Automated: Release ${version}"`);
+execSync('git push');
+if(branch !== 'dev'){
+  console.log(`switching back to branch ${branch}`);
+  execSync(`git switch ${branch}`);
+}
